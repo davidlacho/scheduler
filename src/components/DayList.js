d2a92847eec9dcb474483e744347a8c44966130a
davidlacho <david.lacho@qhrtech.com>
2019-08-24 13:03:11 -0700

    DayList component for rendering DayListItem

---
+++ src/components/DayList.js
@@ -0,0 +1,23 @@
+import React from "react";
+import DayListItem from './DayListItem';
+
+/**
+* DayList component will take in three props.
+
+*** days:Array a list of day objects (each object includes an id, name, and spots)
+*** day:String the currently selected day
+*** setDay:Function accepts the name of the day eg. "Monday", "Tuesday"
+**/
+export default function DayList(props) {
+ return(
+   <ul>
+      {props.days.map(d =>
+      <DayListItem
+       key = {d.id}
+       name={d.name}
+       spots={d.spots}
+       selected={d.name === d.day}
+       setDay={props.setDay}  />)}
+   </ul>
+ );
+}
