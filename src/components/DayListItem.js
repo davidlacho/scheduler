ad03649d5b0d61141d76abd8b120f0b0e04f083a
davidlacho <david.lacho@qhrtech.com>
2019-08-24 12:41:39 -0700

    test cases for text in spots feature

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -3,15 +3,27 @@ import "components/DayListItem.scss";
 import classNames from 'classnames';
 
 export default function DayListItem(props) {
+
+  function formatSpots(spots) {
+    switch(spots) {
+      case 0:
+        return 'no spots remaining';
+      case 1:
+        return '1 spot remaining';
+      default:
+        return `${spots} spots remaining`;
+    }
+  }
+
   var dayClass = classNames({
-    'day-list__item': true, 
+    'day-list__item': true,
     'day-list__item--selected': props.selected === true,
     'day-list__item--full': props.spots === 0
   });
   return(
     <li className = {dayClass} onClick={()=> props.setDay(props.name)}>
       <h2 className="text--regular">{props.name}</h2>
-      <h3 className="text--light">{props.spots}</h3>
+      <h3 className="text--light">{formatSpots(props.spots)}</h3>
     </li>
   );
 
